6163905b8b667789dafc65e7e48b9cfae9e8ea6a
ramiyoukhana7 <ramiprivatmail@gmail.com>
2023-10-02 13:52:03 +0200

    Update App.jsx

--- netflix/src/App.jsx
+++ netflix/src/App.jsx
@@ -1,44 +1,36 @@
-import { useState } from 'react'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-import Home from './views/Home/Home'
-import FilmView from './views/FilmView/FilmView'
-import Favourites from './views/Favourites/Favourites'
-import Categories from './views/Categories/Categories'
-import './App.css'
+import { useState } from "react";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./views/Home/Home";
+import FilmView from "./views/FilmView/FilmView";
+import Favourites from "./views/Favourites/Favourites";
+import Categories from "./views/Categories/Categories";
+import "./App.css";
 
 const router = createBrowserRouter([
-
   {
-    path: '/',
+    path: "/",
     element: <Home />,
   },
   {
-    path: '/FilmView',
-    element: <FilmView />
+    path: "/FilmView",
+    element: <FilmView />,
   },
   {
-    path: '/Favourites',
-    element: <Favourites />
+    path: "/Favourites",
+    element: <Favourites />,
   },
   {
-    path: '/Categories',
-    element: <Categories />
+    path: "/Categories",
+    element: <Categories />,
   },
-])
-
+]);
 
 function App() {
-
-
-
-
-
-
   return (
     <div className="App">
-      <RouterProvider router= { router } />
+      <RouterProvider router={router} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
