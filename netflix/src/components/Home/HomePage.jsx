25c9797ca3dfe0fa7e7b2acfbdb5f09c4f827452
ramiyoukhana7 <ramiprivatmail@gmail.com>
2023-10-03 16:39:53 +0200

    Lagt till kommentarer samt sessionStorage.
    SessionStorage är inte helt färdig än

--- netflix/src/components/Home/HomePage.jsx
+++ netflix/src/components/Home/HomePage.jsx
@@ -8,23 +8,36 @@ export default function HomePage() {
   const [trendingMovies, setTrendingMovies] = useState([]);
   const [recommendedMovies, setRecommendedMovies] = useState([]);
 
+  //Trending funktionen
   function filterTrendingMovies() {
+    //Uppdaterar trendingMovies state med alla filmer som har isTrending prop
     const trending = movies.filter((movie) => movie.isTrending);
     setTrendingMovies(trending);
   }
-
+  //Recommended Funktionen
   function filterRecommendedMovies() {
+    //Uppdaterar recommendedMovies state med alla filmer som inte har isTrending prop
     const recommended = movies.filter((movie) => !movie.isTrending);
     setRecommendedMovies(recommended);
 
+    //Skapar en variabel som kommer hålla 10 slumpmässiga filmer
     const randomMovies = [];
     for (let i = 0; i < 10; i++) {
-      const randomMovie = Math.floor(Math.random() * recommended.length);
-      const recommendedMovies = recommended[randomMovie];
+      //For loopen körs 10 gånger och plockar fram 10 slumpmässiga siffor (filmer) från recommendedMovies array
+      const randomMovieIndex = Math.floor(Math.random() * recommended.length);
+      //Här används siffrorna (filmer) som vi plockade fram för att hämta en film från recommendedMovies array
+      const recommendedMovies = recommended[randomMovieIndex];
+      //Pushar alla våra recommended filmer till randomMovies array
       randomMovies.push(recommendedMovies);
-      console.log(randomMovies);
+      //console.log(randomMovies);
     }
+    //Uppdaterar recommendedMovies state
     setRecommendedMovies(randomMovies);
+  }
+
+  function saveMovies(movie) {
+    sessionStorage.setItem("savedMovies", movie);
+    console.log(sessionStorage);
   }
 
   useEffect(() => {
@@ -42,19 +55,21 @@ export default function HomePage() {
           <article key={movie.title}>
             <img src={movie.thumbnail} />
             <p>Relaste date: {movie.year}</p>
-            <p>rating: {movie.rating}</p>
+            <p>Rating: {movie.rating}</p>
             <h2>{movie.title}</h2>
+            <button onClick={() => saveMovies(movie)}>Save movie</button>
           </article>
         );
       })}
       <h2>Recommended for you</h2>
-      {recommendedMovies.map((movie) => {
+      {recommendedMovies.map((movie, index) => {
         return (
-          <article key={movie.title}>
+          <article key={index}>
             <img src={movie.thumbnail} />
             <p>Relaste date: {movie.year}</p>
-            <p>rating: {movie.rating}</p>
+            <p>Rating: {movie.rating}</p>
             <h2>{movie.title}</h2>
+            <button onClick={() => saveMovies(movie)}>Save movie</button>
           </article>
         );
       })}
