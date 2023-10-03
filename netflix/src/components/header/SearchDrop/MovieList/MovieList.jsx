ad3dddc1c7ab66b24ca56d0abd0727e2b6b078a3
CarlMBergman <113527185+CarlMBergman@users.noreply.github.com>
2023-10-03 09:49:29 +0200

    passing movie to filmview when clicked in search

--- netflix/src/components/header/SearchDrop/MovieList/MovieList.jsx
+++ netflix/src/components/header/SearchDrop/MovieList/MovieList.jsx
@@ -1,10 +1,16 @@
 import './MovieList.css'
+import { useNavigate } from 'react-router-dom'
 
 function MovieList(props) {
     const movie = props.movie
+    const navigate = useNavigate()
+
+    function goToMovie() {
+        navigate('/FilmView', {state: movie})
+    }
 
     return (
-        <article className='movie--list'>
+        <article className='movie--list' onClick={ goToMovie }>
             <img className='movie--list__img' src={ movie.thumbnail } alt={ movie.title } />
             <div>
                 <h3 className='movie--list__title'>{ movie.title }</h3>
