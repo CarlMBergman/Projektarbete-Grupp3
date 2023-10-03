7608c1a9716d0152fa9ef88e56ed35d2344317f9
Ivan Hamou <70196210+IvanHamou@users.noreply.github.com>
2023-10-03 12:02:00 +0200

    Set up favourites and created component for it

---
+++ netflix/src/components/favourites/FavouriteCard.jsx
@@ -0,0 +1,17 @@
+import "./FavouriteCard.css"
+
+function FavouriteCard() {
+    return (
+        <div>
+            <section>
+                <article className="favourite_article">test</article>
+                <article className="favourite_article"></article>
+                <article className="favourite_article"></article>
+                <article className="favourite_article"></article>
+                <article className="favourite_article"></article>
+            </section>
+        </div>
+    )
+}
+
+export default FavouriteCard
