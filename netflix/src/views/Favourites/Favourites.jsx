dd858af0d02c5489448ccd73b7abba46b6f73849
Ivan Hamou <70196210+IvanHamou@users.noreply.github.com>
2023-10-04 13:18:57 +0200

    working on adding multiple movies to favourite

--- netflix/src/views/Favourites/Favourites.jsx
+++ netflix/src/views/Favourites/Favourites.jsx
@@ -1,14 +1,25 @@
+import { useState } from "react";
 import FavouriteCard from "../../components/favourites/FavouriteCard.jsx"
 import Header from "../../components/header/Header.jsx"
 import "./Favourites.css"
 
+
 function Favourites() {
+
+    const [favouriteCards, setFavouriteCards] = useState([]);
+
+    const handleAddToFavorites = () => {
+        const newCard = <FavouriteCard key={favouriteCards.length} />;
+        setFavouriteCards([...favouriteCards, newCard]);
+    }
+
     return(
         <div>
             <Header/>
             <main>
                 <h4>Favourites</h4>
                 <section>
+                    {favouriteCards}
                     <FavouriteCard/>
                 </section>
             </main>
