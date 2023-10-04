dc6f2eda1e58fcdde66f4dbb575d0cebb0577f80
CarlMBergman <113527185+CarlMBergman@users.noreply.github.com>
2023-10-04 14:28:53 +0200

    made movies go in to categories

--- netflix/src/views/Categories/Categories.jsx
+++ netflix/src/views/Categories/Categories.jsx
@@ -1,15 +1,68 @@
 import './Categories.css'
 import { Category } from '../../components/category/Category'
 import Header from '../../components/header/Header'
+import movies from '../../movies.json'
 
 function Categories() {
+    let genres = []
+    const comma = ','
+
+    movies.forEach((movie) => {
+        if (movie.genre.includes(comma)) {
+            const noSpaces = movie.genre.replace(/\s/g, '')
+            const splitGenres = noSpaces.split(comma)
+            splitGenres.forEach((genre) => {
+                if (genres) {
+                    const exist = genres.includes(genre)
+                    if (!exist) {
+                        genres = [...genres, genre]
+                    }
+                } else {
+                    genres = genre
+                }
+            })
+        } else {
+            const exist = genres.includes(movie.genre)
+            if (!exist) {
+                genres = [...genres, movie.genre]
+            }
+        }
+    })
+
+    const category = genres.map((genre) => {
+        let moviesWithSameGenre = []
+        movies.forEach((movie) => {
+            if (movie.genre.includes(comma)) {
+                const noSpaces = movie.genre.replace(/\s/g, '')
+                const splitGenres = noSpaces.split(comma)
+                splitGenres.forEach((genreMovie) => {
+                    if (genreMovie === genre) {
+                        moviesWithSameGenre = [...moviesWithSameGenre, movie]
+                        
+                    }
+                })
+            } else {
+                if (genre === movie.genre) {
+                    moviesWithSameGenre = [...moviesWithSameGenre, movie]
+                }
+            }
+        })
+        
+        return (
+            <div key={genre}>
+                <h2>{genre}</h2>
+                <Category movies={moviesWithSameGenre}/>
+            </div>
+        )
+    })
+
     return(
         <div>
             <header>
                 < Header/>  
             </header>
             <main className="categories">
-                <Category />
+                {category}
             </main>
         </div>
     )
