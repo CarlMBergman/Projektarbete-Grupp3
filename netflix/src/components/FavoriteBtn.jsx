d91806e4bf74220e47ddc8f954aed3d61ab7a65b
ramiyoukhana7 <ramiprivatmail@gmail.com>
2023-10-04 15:21:12 +0200

    Uppdaterade sessionStorage för att kunna spara flera filmer

--- netflix/src/components/FavoriteBtn.jsx
+++ netflix/src/components/FavoriteBtn.jsx
@@ -1,6 +1,14 @@
 function FavoriteBtn(movie) {
-  sessionStorage.setItem("savedMovies", JSON.stringify(movie));
-  console.log(sessionStorage);
+  const savedMovies = [];
+  const test = JSON.parse(sessionStorage.getItem("savedMovies"));
+  console.log(test);
+  if (test) {
+    const updated = [...test, movie];
+    console.log(updated);
+    sessionStorage.setItem("savedMovies", JSON.stringify(updated));
+  } else {
+    sessionStorage.setItem("savedMovies", JSON.stringify([movie]));
+  }
 
   return <button onClick={() => FavoriteBtn(movie)}>Save Movie</button>;
 }
