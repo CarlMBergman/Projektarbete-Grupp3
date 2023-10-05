49a40eb7fa27a9973ea8e4f35cb1b7bb00d3f2f8
CarlMBergman <113527185+CarlMBergman@users.noreply.github.com>
2023-10-05 13:21:49 +0200

    added a beforeEach render in tests

--- netflix/src/testing/Categories.test.jsx
+++ netflix/src/testing/Categories.test.jsx
@@ -1,21 +1,24 @@
-import { describe, it, expect } from "vitest";
+import { describe, it, expect, beforeEach } from "vitest";
 import { render, screen } from "@testing-library/react";
 import userEvent from '@testing-library/user-event'
 import Categories from "../views/Categories/Categories";
 import { BrowserRouter } from "react-router-dom";
 
+beforeEach(() => {
+    render(<Categories/>, {wrapper: BrowserRouter})
+})
+
 describe('tests for categories', () => {
+
     it('should display 15 different categories', () => {
-        render(<Categories/>, {wrapper: BrowserRouter})
         expect(screen.getAllByRole('heading', {level: 2})).toHaveLength(15)
     })
 
     it('should show The Dark Knight in 3 different categories', () => {
-        render(<Categories/>, {wrapper: BrowserRouter})
         expect(screen.getAllByText('The Dark Knight')).toHaveLength(3)
     })
 
-    it('should show The Shawshank Redemption only one time', () => {
-        expect(screen.getByText('The Shawshank Redemption')).toBeInTheDocument()
+    it('should show Fight Club only one time', () => {
+        expect(screen.getByText('Fight Club')).toBeInTheDocument()
     })
 })
