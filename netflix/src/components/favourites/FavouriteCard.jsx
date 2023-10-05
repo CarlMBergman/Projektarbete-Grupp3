9f5275a87dd294716bdb86cf8b67940b9f781afa
Ivan Hamou <70196210+IvanHamou@users.noreply.github.com>
2023-10-05 10:37:57 +0200

    can add multiple movies to favourite

--- netflix/src/components/favourites/FavouriteCard.jsx
+++ netflix/src/components/favourites/FavouriteCard.jsx
@@ -1,23 +1,11 @@
-import { useState, useEffect } from "react";
+import React from "react";
 import "./FavouriteCard.css"
 
-function FavouriteCard() {
-
-    const [movieData, setMovieData] = useState([])
-
-    useEffect(() => {
-        function getMovie() {
-            let data = JSON.parse(sessionStorage.getItem("savedMovies"));
-            console.log(data);
-            setMovieData(data);
-        }
-        getMovie();
-    }, []);
-
+function FavouriteCard({ movieData }) {
     return (
         <div>
             <article className="favourite_article">
-                <img src={movieData.thumbnail} alt="thumbnail"/>
+                <img src={movieData.thumbnail} alt="thumbnail" />
                 <h3>{movieData.title}</h3>
                 <p>{movieData.synopsis}</p>
             </article>
@@ -25,4 +13,4 @@ function FavouriteCard() {
     )
 }
 
-export default FavouriteCard
+export default FavouriteCard;
