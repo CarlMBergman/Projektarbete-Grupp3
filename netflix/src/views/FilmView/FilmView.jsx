615b2cec249b6625389a7ffee66c0497b9e300f8
Anton Sjöstrand <asjoa99@hotmail.com>
2023-10-09 22:23:13 +0200

    Försökt mig på litet tester

--- netflix/src/views/FilmView/FilmView.jsx
+++ netflix/src/views/FilmView/FilmView.jsx
@@ -4,7 +4,7 @@ import Header from '../../components/header/Header'
 import { useLocation } from 'react-router-dom';
 import removeMovieByTitle from '../../components/RemoveFavoriteBtn';
 import FavoriteBtn from '../../components/FavoriteBtn';
-
+import { BrowserRouter } from 'react-router-dom';
 
 function FilmView() {
 
