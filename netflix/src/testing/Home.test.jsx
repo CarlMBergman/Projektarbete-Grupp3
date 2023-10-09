de99c262882516853642eb3019fd42007f08a228
ramiyoukhana7 <ramiprivatmail@gmail.com>
2023-10-09 13:35:57 +0200

    Flyttade testning, försöker lösa legacy error

--- netflix/src/testing/Home.test.jsx
+++ netflix/src/testing/Home.test.jsx
@@ -1,7 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 
-import App from "../../App";
+import App from "../App";
 
 describe("App", () => {
   test("Should display trending movies", () => {
