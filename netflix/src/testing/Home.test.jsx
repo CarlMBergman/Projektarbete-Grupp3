dc8e2ba4d3921686c2b702b955f4a160b573ae9a
ramiyoukhana7 <ramiprivatmail@gmail.com>
2023-10-11 11:07:42 +0200

    Ändrade lite CSS

--- netflix/src/testing/Home.test.jsx
+++ netflix/src/testing/Home.test.jsx
@@ -1,8 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import { describe, expect, test, beforeEach } from "vitest";
-
 import userEvent from "@testing-library/user-event";
-
 import Home from "../views/Home/Home";
 import { BrowserRouter } from "react-router-dom";
 
